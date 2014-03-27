6a07a2a9471cb6be86da171d77c95220735b3758
Hack Reactor Students <github@hackreactor.com>
2014-03-27 11:57:05 -0700

    some deployment errors, changed the port to be dynamic

---
+++ azure-setup-notes.md
@@ -0,0 +1,5 @@
+tried whole procedure again
+tried to log in different regions
+
+tried from azure online UI
+The resource you are looking for has been removed, had its name changed, or is temporarily unavailable.
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var app = require('./server-config.js');
 
-var port = 4568;
+var port = process.env.PORT || 4568;
 
 app.listen(port);
+
+console.log('domain:  ' , app.get('domain'));
 
 console.log('Server now listening on port ' + port);
